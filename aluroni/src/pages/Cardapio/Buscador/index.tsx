dd46da2afbbb682bf6a272bed40fee0ccbd1b669
mtavidal <119874263+mtavidal@users.noreply.github.com>
2023-07-17 11:27:26 -0300

    Adiciona header e buscador

---
+++ aluroni/src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./Buscador.module.scss";
+import { CgSearch } from "react-icons/cg";
+
+interface BuscadorProps {
+  busca: string;
+  setBusca: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Buscador({ setBusca, busca }: BuscadorProps) {
+  return (
+    <div className={styles.buscador}>
+      <input
+        type="text"
+        value={busca}
+        onChange={(evento) => setBusca(evento.target.value)}
+      />
+      <CgSearch size={20} color="#4C4D5E" />
+    </div>
+  );
+}
