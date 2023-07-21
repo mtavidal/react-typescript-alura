a6b11560e274aad82c0e030df7413fe56dfa2cd2
mtavidal <119874263+mtavidal@users.noreply.github.com>
2023-07-21 16:20:30 -0300

    Adiciona funções de deletar, atualizar e filtrar restaurante

--- alfood-main/src/paginas/AdministracaoRestaurantes/index.tsx
+++ alfood-main/src/paginas/AdministracaoRestaurantes/index.tsx
@@ -6,10 +6,12 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Button,
 } from '@mui/material';
 import IRestaurante from '../../interfaces/IRestaurante';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function AdministracaoRestaurantes() {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
@@ -25,18 +27,48 @@ export default function AdministracaoRestaurantes() {
       });
   }, []);
 
+  function excluirRestaurante(restauranteAserExcluido: IRestaurante) {
+    axios
+      .delete(
+        `http://localhost:8000/api/v2/restaurantes/${restauranteAserExcluido.id}/`
+      )
+      .then(() => {
+        const listaRestaurante = restaurantes.filter(
+          (restaurante) => restaurante.id !== restauranteAserExcluido.id
+        );
+        setRestaurantes([...listaRestaurante]);
+      });
+  }
+
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell>Nome</TableCell>
+            <TableCell>Editar</TableCell>
+            <TableCell>Excluir</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {restaurantes.map((restaurante) => (
             <TableRow key={restaurante.id}>
               <TableCell>{restaurante.nome}</TableCell>
+              <TableCell>Nome</TableCell>
+              <TableCell>
+                [
+                <Link to={`/admin/restaurantes/${restaurante.id}`}>editar</Link>{' '}
+                ]
+              </TableCell>
+              <TableCell>
+                <Button
+                  onClick={() => excluirRestaurante(restaurante)}
+                  variant="outlined"
+                  color="error"
+                >
+                  Excluir
+                </Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
