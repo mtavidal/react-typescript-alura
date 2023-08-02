e4ec4cb68820fa8fd0fca7906671f31fb00992b3
mtavidal <119874263+mtavidal@users.noreply.github.com>
2023-08-02 20:54:38 -0300

    Adiciona card e estilos, estilo formulario, testes da lista de participantes e do rodape

--- sorteador-de-amigo-secreto/src/App.tsx
+++ sorteador-de-amigo-secreto/src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import Configuracao from './paginas/Configuracao';
 
 function App() {
-  return <>teste</>;
+  return (
+    <BrowserRouter>
+      <RecoilRoot>
+        <Routes>
+          <Route path="/" element={<Configuracao />} />
+        </Routes>
+      </RecoilRoot>
+    </BrowserRouter>
+  );
 }
 
 export default App;
